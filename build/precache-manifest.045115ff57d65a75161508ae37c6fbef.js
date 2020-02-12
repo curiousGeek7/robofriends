425f268cff21897e23f9593b6e65e7d5cf07a5df
CuriousGeek <krit.s255@gmail.com>
2020-02-12 04:41:10 -0500

    Publish to github pages

---
+++ build/precache-manifest.045115ff57d65a75161508ae37c6fbef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5edd445cd5bbc5c5dee8696822f8f16",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b5c6731c155b3ee215a3",
+    "url": "/robofriends/static/css/main.c355da90.chunk.css"
+  },
+  {
+    "revision": "db030a0313823e763c87",
+    "url": "/robofriends/static/js/2.fbad7c07.chunk.js"
+  },
+  {
+    "revision": "b5c6731c155b3ee215a3",
+    "url": "/robofriends/static/js/main.5abe50b2.chunk.js"
+  },
+  {
+    "revision": "15646e7ae3f7e7d4e926",
+    "url": "/robofriends/static/js/runtime-main.59eca5ac.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
